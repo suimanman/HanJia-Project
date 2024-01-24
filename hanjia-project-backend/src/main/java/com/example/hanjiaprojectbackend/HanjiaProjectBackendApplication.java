c027252a8884ef083d80795abc770a7c2c1c7954
suimanman <1847515809@qq.com>
2024-01-24 15:26:52 +0800

    创建项目

---
+++ hanjia-project-backend/src/main/java/com/example/hanjiaprojectbackend/HanjiaProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanjiaprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanjiaProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanjiaProjectBackendApplication.class, args);
+    }
+
+}
